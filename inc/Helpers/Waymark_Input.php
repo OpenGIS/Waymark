ff27a132c50ac5dd4db20e37d63e06407e2b2a92
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2025-01-14 08:33:38 +0000

    moved double quotes

--- inc/Helpers/Waymark_Input.php
+++ inc/Helpers/Waymark_Input.php
@@ -73,11 +73,11 @@ class Waymark_Input {
 				$field['tip'] .= '.';
 			}
 
-			$out .= ' <a data-title="' . esc_attr__($field['tip']) . '"';
+			$out .= ' <a data-title="' . esc_attr__($field['tip']) . '';
 			if (array_key_exists('tip_link', $field)) {
-				$out .= ' ' . esc_attr__('Click here for more details.', 'waymark') . ' href="' . $field['tip_link'] . '" target="_blank" class="waymark-tooltip waymark-link"';
+				$out .= ' ' . esc_attr__('Click here for more details.', 'waymark') . '" href="' . $field['tip_link'] . '" target="_blank" class="waymark-tooltip waymark-link"';
 			} else {
-				$out .= ' href="#" onclick="return false;" class="waymark-tooltip"';
+				$out .= '" href="#" onclick="return false;" class="waymark-tooltip"';
 			}
 			$out .= '>?</a>';
 		}
