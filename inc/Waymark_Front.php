df15250e7d034047fc025fe90452dff4cabd333c
Joe Hawes <morehawes@gmail.com>
2025-01-23 16:58:58 -0800

    10 - Front

--- inc/Waymark_Front.php
+++ inc/Waymark_Front.php
@@ -3,25 +3,25 @@
 class Waymark_Front {
 	function __construct() {
 		//Don't do anything if we're in the back-end
-		if(is_admin()) {
+		if (is_admin()) {
 			return;
 		}
-		
-		add_action('init', array($this, 'init'));
-		add_action('wp_head', array($this, 'wp_head'));			
+
+		add_action('init', [$this, 'init']);
+		add_action('wp_head', [$this, 'wp_head']);
 	}
-	
+
 	function init() {
-		require_once('Front/Waymark_JS.php');
-		require_once('Front/Waymark_CSS.php');
-		require_once('Front/Waymark_Shortcode.php');
-		require_once('Front/Waymark_Content.php');
-		require_once('Front/Waymark_HTTP.php');
-		require_once('Front/Waymark_Submission.php');
+		require_once 'Front/Waymark_JS.php';
+		require_once 'Front/Waymark_CSS.php';
+		require_once 'Front/Waymark_Shortcode.php';
+		require_once 'Front/Waymark_Content.php';
+		require_once 'Front/Waymark_HTTP.php';
+		require_once 'Front/Waymark_Submission.php';
 	}
 
 	function wp_head() {
-		echo '<meta name="' . Waymark_Config::get_name(true, true) . ' Version" content="' . Waymark_Config::get_version() . '" />' . "\n";	
+		echo '<meta name="' . esc_attr(Waymark_Config::get_name(true, true)) . ' Version" content="' . esc_attr(Waymark_Config::get_version()) . '" />' . "\n";
 	}
-}	
+}
 new Waymark_Front;
